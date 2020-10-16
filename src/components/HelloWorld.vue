72a9a0e6bf1b726a08b4f16b1fd4367f54c19af6
goyoungsang <sang-2454>
2020-10-16 14:43:11 +0900

    11

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,16 @@
 <template>
     <v-container fluid>
         <v-card>
-          <v-toolbar dense flat>
+          <v-toolbar dense flat color="primary" dark>
             <v-toolbar-title>문서편집기</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>mdi-eye</v-icon>
+            <v-btn icon @click="editMode = !editMode">
+              <v-icon>{{editMode ? 'mdi-eye' : 'mdi-pencil'}}</v-icon>
             </v-btn>
           </v-toolbar>
           <v-card-text>
-            <v-textarea v-model="text"></v-textarea>
+            <Editor v-if="editMode" ref="toastuiEditor" initialEditType="wysiwyg" v-model="text"/>
+            <Viewer v-if="!editMode && text != ''" :initialValue="text"/>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
@@ -21,12 +22,22 @@
 </template>
 
 <script>
+import 'codemirror/lib/codemirror.css'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import { Editor, Viewer } from '@toast-ui/vue-editor'
+
 const { dialog } = require('electron').remote
 const fs = require('fs')
+
 export default {
+  components: {
+    Editor,
+    Viewer
+  },
   data () {
     return {
-      text: ''
+      text: '',
+      editMode: true
     }
   },
   mounted () {
@@ -45,7 +56,9 @@ export default {
       }
       const r = dialog.showOpenDialogSync(options)
       if (!r) return
-      this.text = fs.readFileSync(r[0])
+      this.text = fs.readFileSync(r[0]).toString()
+      console.log(this.text)
+      this.$refs.toastuiEditor.invoke('setHtml', this.text)
     },
     write () {
       const options = {
@@ -57,6 +70,11 @@ export default {
           }
         ]
       }
+      console.log('---------')
+      console.log(this.text)
+      console.log(this.$refs.toastuiEditor.invoke('getHtml'))
+      console.log('---------')
+      this.text = this.$refs.toastuiEditor.invoke('getHtml')
       const r = dialog.showSaveDialogSync(options)
       if (!r) return
       fs.writeFileSync(r, this.text)
